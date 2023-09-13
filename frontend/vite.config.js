a1e2c979dd014f66160061f779fa49197843ce57
Mayank Sandilya <78838306+beingkamikaze@users.noreply.github.com>
2023-09-14 05:03:13 +0530

    frontend

---
+++ frontend/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    // Get rid of the CORS error
+    proxy: {
+      "/api": {
+        target: "https://mt-89vs.onrender.com/",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
